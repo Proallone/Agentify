48f1177e2f76a533589d3df805e9dac3c238f63c
Proallone <proallone14@gmail.com>
2021-10-09 09:13:28 +0200

    Add registration and login
    added firebase backed registration and login functionality

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,60 +1,122 @@
-/* ŻRÓDŁO https://reactnativemaster.com/react-native-login-screen-tutorial/ */
+// components/login.js
+//https://www.positronx.io/react-native-firebase-login-and-user-registration-tutorial/
 
-import React from "react";
-import {
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ActivityIndicator } from 'react-native';
+import firebase from '../database/Firebase';
 
-import { style } from "../styles/Index";
-import colors from "../assets/colors/Colors";
 
-export default class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
-  };
+export default class Login extends Component {
+  
+  constructor() {
+    super();
+    this.state = { 
+      email: '', 
+      password: '',
+      isLoading: false
+    }
+  }
+
+  updateInputVal = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
+
+  userLogin = () => {
+    if(this.state.email === '' && this.state.password === '') {
+      alert('Enter details to signin!')
+    } else {
+      this.setState({
+        isLoading: true,
+      })
+      firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((res) => {
+        console.log(res)
+        console.log('User logged-in successfully!')
+        this.setState({
+          isLoading: false,
+          email: '', 
+          password: ''
+        })
+        this.props.navigation.navigate('Main')
+      })
+      .catch(error => this.setState({ errorMessage: error.message }))
+    }
+  }
+
   render() {
-    const { navigate } = this.props.navigation;
+    if(this.state.isLoading){
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicator size="large" color="#9E9E9E"/>
+        </View>
+      )
+    }    
     return (
-      <View style={style.container}>
-        <View style={style.logoContainer}>
-        <Text style={style.logo}>Witaj!</Text>
-        <Image
-              style={style.tinyLogo}
-              source={require("../assets/images/icon_main.png")}
+      <View style={styles.container}>  
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Email"
+          value={this.state.email}
+          onChangeText={(val) => this.updateInputVal(val, 'email')}
         />
-        </View>
-        <View style={style.inputView}>
-          <TextInput
-            style={style.inputText}
-            placeholder="Email..."
-            placeholderTextColor={colors.white}
-            onChangeText={(text) => this.setState({ email: text })}
-          />
-        </View>
-        <View style={style.inputView}>
-          <TextInput
-            secureTextEntry
-            style={style.inputText}
-            placeholder="Hasło..."
-            placeholderTextColor={colors.white}
-            onChangeText={(text) => this.setState({ password: text })}
-          />
-        </View>
-        <TouchableOpacity>
-          <Text style={style.forgot} onPress={()=> navigate("Forgot")}>Zapomniałeś hasła?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.loginBtn} onPress={()=> navigate("Main")}>
-          <Text style={style.loginText}>Zaloguj</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=> navigate("Register")}>
-          <Text style={style.signUpText}>Zarejestruj się!</Text>
-        </TouchableOpacity>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Password"
+          value={this.state.password}
+          onChangeText={(val) => this.updateInputVal(val, 'password')}
+          maxLength={15}
+          secureTextEntry={true}
+        />   
+        <Button
+          color="#3740FE"
+          title="Signin"
+          onPress={() => this.userLogin()}
+        />   
+
+        <Text 
+          style={styles.loginText}
+          onPress={() => this.props.navigation.navigate('Register')}>
+          Don't have account? Click here to signup
+        </Text>                          
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 35,
+    backgroundColor: '#fff'
+  },
+  inputStyle: {
+    width: '100%',
+    marginBottom: 15,
+    paddingBottom: 15,
+    alignSelf: "center",
+    borderColor: "#ccc",
+    borderBottomWidth: 1
+  },
+  loginText: {
+    color: '#3740FE',
+    marginTop: 25,
+    textAlign: 'center'
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  }
+});
