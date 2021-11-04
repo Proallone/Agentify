5ad9631e306328de235a2b498af8fdba63d6b1a9
Proallone <46166114+Proallone@users.noreply.github.com>
2021-11-04 22:13:23 +0100

    refactor
    huh

--- src/screens/SearchClient.js
+++ src/screens/SearchClient.js
@@ -1,71 +1,50 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-import { DataTable } from 'react-native-paper';
-import { firebaseGetDocs } from '../services/FirebaseMethods';
+import { Text, ScrollView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import { DataTable } from "react-native-paper";
+import { firebaseGetDocs } from "../services/FirebaseMethods";
 
-export default class Reset extends Component {
+export default class ClientSearch extends Component {
   constructor() {
     super();
     this.state = {
-      clients: []
+      clients: [],
     };
-    this.dupa();
+    this.populateClientsArray();
   }
 
-  dupa = async () => {
+  populateClientsArray = async () => {
     this.setState({
       clients: await firebaseGetDocs(),
-    })
-  }
-  
+    });
+  };
 
-  
   render() {
-
-  return (
-    <View style={styles.container}>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title>client</DataTable.Title>
-          <DataTable.Title>Code</DataTable.Title>
-          <DataTable.Title>
-            Balance Available
-          </DataTable.Title>
-        </DataTable.Header>
-        {
-          this.state.clients.map(client => {
-          return (
-            <DataTable.Row
-              key={client.id}
-              onPress={() => {
-                console.log(`selected client ${client.id}`)
-              }}
-            >
-              <DataTable.Cell>
-                {client.name}
-              </DataTable.Cell>
-              <DataTable.Cell>
-                {client.surname}
-              </DataTable.Cell>
-              <DataTable.Cell numeric>
-                {client.PESEL}
-              </DataTable.Cell>
-            </DataTable.Row>
-        )})}
-      </DataTable>
-    </View>
-  );
-            }
+    return (
+      <ScrollView>
+        <DataTable>
+          <DataTable.Header>
+            <DataTable.Title>Imie</DataTable.Title>
+            <DataTable.Title>Nazwisko</DataTable.Title>
+            <DataTable.Title>PESEL</DataTable.Title>
+          </DataTable.Header>
+          {this.state.clients.map((client) => {
+            return (
+              <DataTable.Row
+                key={client.id}
+                onPress={() => {
+                  console.log(`selected client ${client.id}`);
+                }}
+              >
+                <DataTable.Cell>{client.name}</DataTable.Cell>
+                <DataTable.Cell>{client.surname}</DataTable.Cell>
+                <DataTable.Cell numeric>{client.PESEL}</DataTable.Cell>
+              </DataTable.Row>
+            );
+          })}
+        </DataTable>
+      </ScrollView>
+    );
+  }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-});
